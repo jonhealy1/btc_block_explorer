7c51bf9cf2ab212273ac62345ee7ace8bc39ce77
noonespecial009 <computer@w092-017.guest.wireless.uvic.ca>
2019-08-31 15:39:14 -0700

    clean up

--- block_viewer/block_viewer.go
+++ block_viewer/block_viewer.go
@@ -94,10 +94,7 @@ func main() {
 	fmt.Println("merkle root: ", head.merkleRoot)
 	timeStamp := fromHex(head.timeStamp)
 	fmt.Println("timestamp: ", timeStamp, "(unix time)")
-	// date, err := strconv.ParseInt("1405544146", 10, 64)
-    // if err != nil {
-    //     panic(err)
-    // }
+
     timeNotUnix := time.Unix(int64(timeStamp), 0)
     fmt.Println("timestamp: ", timeNotUnix, "(converted)")
 	fmt.Println("target difficulty: ", head.targetDiff)
@@ -106,97 +103,91 @@ func main() {
 	fmt.Println("number of transactions: ", fromHex(head.numTransactions))
 
 	////////////////start transaction loop////////////////////////////////
-	/* Transaction Version # */
 
 	var transactions [15]transaction
 
 	for i:=1; i<=5; i++ {
 
-	transactions[i].transVersion = convertEndian(string(testBlock[byteC:byteC+8]))
-	byteC += 8
+		transactions[i].transVersion = convertEndian(string(testBlock[byteC:byteC+8]))
+		byteC += 8
 
-	//////////////////////start number of inputs loop////////////////////
+		//////////////////////start number of inputs loop////////////////////
 	 
-	transactions[i].numInputs = convertEndian(string(testBlock[byteC:byteC+2]))
-	byteC += 2
+		transactions[i].numInputs = convertEndian(string(testBlock[byteC:byteC+2]))
+		byteC += 2
 
-	for k:=1;k<=fromHex(transactions[i].numInputs);k++ {
-		//fmt.Println("***Input ", i, "***")
-		transactions[i].prevTrans[k] = convertEndian(string(testBlock[byteC:byteC+64]))
-		byteC += 64
-		transactions[i].transIndex = convertEndian(string(testBlock[byteC:byteC+8]))
-		byteC += 8
-		//account for variable langth
-		scriptLengthVar := convertEndian(string(testBlock[byteC:byteC+2]))
-		//fmt.Println(scriptLengthVar)
-    	if scriptLengthVar!="fd" && scriptLengthVar!="fe" && scriptLengthVar!="ff" {
-			transactions[i].scriptLength = convertEndian(string(testBlock[byteC:byteC+2]))
-			byteC += 2
-		} else {
-			jump := varLength(scriptLengthVar)
-			transactions[i].scriptLength = convertEndian(string(testBlock[byteC+2:byteC+jump]))
-			byteC += jump
+		for k:=1;k<=fromHex(transactions[i].numInputs);k++ {
+			//fmt.Println("***Input ", i, "***")
+			transactions[i].prevTrans[k] = convertEndian(string(testBlock[byteC:byteC+64]))
+			byteC += 64
+			transactions[i].transIndex = convertEndian(string(testBlock[byteC:byteC+8]))
+			byteC += 8
+			//account for variable langth
+			scriptLengthVar := convertEndian(string(testBlock[byteC:byteC+2]))
+			//fmt.Println(scriptLengthVar)
+    		if scriptLengthVar!="fd" && scriptLengthVar!="fe" && scriptLengthVar!="ff" {
+				transactions[i].scriptLength = convertEndian(string(testBlock[byteC:byteC+2]))
+				byteC += 2
+			} else {
+				jump := varLength(scriptLengthVar)
+				transactions[i].scriptLength = convertEndian(string(testBlock[byteC+2:byteC+jump]))
+				byteC += jump
+			}
+			byteC += fromHex(transactions[i].scriptLength)*2
+			byteC += 8
 		}
-		byteC += fromHex(transactions[i].scriptLength)*2
-		byteC += 8
-		
-	}
 
-	//////////////////////end number of inputs loop////////////////////
+		//////////////////////end number of inputs loop////////////////////
 
-	transactions[i].numOutputs = convertEndian(string(testBlock[byteC:byteC+2]))
-	byteC += 2
+		transactions[i].numOutputs = convertEndian(string(testBlock[byteC:byteC+2]))
+		byteC += 2
 
-	fmt.Println("-------------------------------")
-	fmt.Println("Transaction", i, "Inputs")
-	fmt.Println("-------------------------------")
+		fmt.Println("-------------------------------")
+		fmt.Println("Transaction", i, "Inputs")
+		fmt.Println("-------------------------------")
 
-	fmt.Println("version number: ", fromHex(transactions[i].transVersion))
-	numberInputs := fromHex(transactions[i].numInputs)
-	fmt.Println("number of inputs: ", numberInputs)
-	///previous transactions loop start/////////
-	for p:=1; p<=numberInputs; p++ {
-		fmt.Println("previous transaction",p, ":", transactions[i].prevTrans[p])
-	}
-	/////////////////start of outputs loop////////////////
+		fmt.Println("version number: ", fromHex(transactions[i].transVersion))
+		numberInputs := fromHex(transactions[i].numInputs)
+		fmt.Println("number of inputs: ", numberInputs)
+		///previous transactions loop start/////////
+		for p:=1; p<=numberInputs; p++ {
+			fmt.Println("previous transaction",p, ":", transactions[i].prevTrans[p])
+		}
+		/////////////////start of outputs loop////////////////
 
-	for j:=1;j<=fromHex(transactions[i].numOutputs);j++ {
-	transactions[i].amountBTC = convertEndian(string(testBlock[byteC:byteC+16]))
-	byteC += 16
-	transactions[i].pkScript_length = convertEndian(string(testBlock[byteC:byteC+2]))
-	byteC += 2
-	//fmt.Println(string(testBlock[byteC:byteC+6]))
-	byteC += 6 //???????????????
-	jump := fromHex(transactions[i].pkScript_length) *2 -6 -4
-	transactions[i].pkScript = string(testBlock[byteC:byteC+jump])
-	byteC += jump
-	//fmt.Println(convertEndian(string(testBlock[byteC:byteC+4]))) //lock time + 88ac
-	byteC += 4
-	numberOutputs := fromHex(transactions[i].numOutputs)
-	if j==numberOutputs {
-		byteC += 8 //lock time - just at end of outputs
-	}
-	fmt.Println("-------------------------------")
-	fmt.Println("Transaction", i, "Output", j, "/", numberOutputs )
-	fmt.Println("-------------------------------")
+		for j:=1;j<=fromHex(transactions[i].numOutputs);j++ {
+			transactions[i].amountBTC = convertEndian(string(testBlock[byteC:byteC+16]))
+			byteC += 16
+			transactions[i].pkScript_length = convertEndian(string(testBlock[byteC:byteC+2]))
+			byteC += 2
+			//fmt.Println(string(testBlock[byteC:byteC+6]))
+			byteC += 6 //???????????????
+			jump := fromHex(transactions[i].pkScript_length) *2 -6 -4
+			transactions[i].pkScript = string(testBlock[byteC:byteC+jump])
+			byteC += jump
+			//fmt.Println(convertEndian(string(testBlock[byteC:byteC+4]))) //lock time + 88ac
+			byteC += 4
+			numberOutputs := fromHex(transactions[i].numOutputs)
+			if j==numberOutputs {
+				byteC += 8 //lock time - just at end of outputs
+			}
+			fmt.Println("-------------------------------")
+			fmt.Println("Transaction", i, "Output", j, "/", numberOutputs )
+			fmt.Println("-------------------------------")
 	
-	//fmt.Println("previous transaction1: ", transactions[i].prevTrans[1])
-	//fmt.Println("previous transaction2: ", transactions[i].prevTrans[2])
-	//100 inputs: 000000000000000001643f7706f3dcbc3a386e4c1bfba852ff628d8024f875b6
+			//100 inputs: 000000000000000001643f7706f3dcbc3a386e4c1bfba852ff628d8024f875b6
 
-	///previous transactions loop end///////
-	fmt.Println("transaction index: ", transactions[i].transIndex)
-	fmt.Println("script length: ", fromHex(transactions[i].scriptLength), "bytes")
-	fmt.Println("number of outputs: ", numberOutputs)
-	fmt.Println("amount: ", fromHex(transactions[i].amountBTC))
-	fmt.Println("amount: ", transactions[i].amountBTC)
-	fmt.Println("amount: ", float64(fromHex(transactions[i].amountBTC))/100000000, "BTC")
-	fmt.Println("pk_script length: ", fromHex(transactions[i].pkScript_length), "bytes")
-	fmt.Println("receiver address: ", transactions[i].pkScript, "(hash 160)")
-	}
-	//////////////end of outputs loop////////////////
-
-	
+			///previous transactions loop end///////
+			fmt.Println("transaction index: ", transactions[i].transIndex)
+			fmt.Println("script length: ", fromHex(transactions[i].scriptLength), "bytes")
+			fmt.Println("number of outputs: ", numberOutputs)
+			fmt.Println("amount: ", fromHex(transactions[i].amountBTC))
+			fmt.Println("amount: ", transactions[i].amountBTC)
+			fmt.Println("amount: ", float64(fromHex(transactions[i].amountBTC))/100000000, "BTC")
+			fmt.Println("pk_script length: ", fromHex(transactions[i].pkScript_length), "bytes")
+			fmt.Println("receiver address: ", transactions[i].pkScript, "(hash 160)")
+		}
+		//////////////end of outputs loop////////////////
 
 	}
 	////////////////end transaction loop////////////////////////////////
